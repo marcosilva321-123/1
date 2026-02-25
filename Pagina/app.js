69c7c5c6c0a4c068522464ef30b09b37ad6c03f1
marcosilva321-123 <marcosilva321@aragon.unam.mx>
2026-02-25 10:09:10 -0600

    commit

---
+++ Pagina/app.js
@@ -0,0 +1,36 @@
+//alert ("Hola 1")
+//alert ("Hola 2")
+//alert ("Hola 3")
+
+/*console.log("Marco");
+console.log("a");
+
+console.log(5)
+console.log(5+5)
+console.log(2,3)
+console.log(true)
+
+var nombre = "Marco"
+var nombre =true
+console.log(nombre)
+
+let apellido = "Silva"
+console.log(apellido)
+const edad=20
+console.log(edad)
+
+let dato = 41242333
+dato = '32'
+
+const port =3306
+
+var dato1 = 'Hola'
+var dato2 = 'mundo'
+var dato3 = ' '
+var res = dato1+dato3+dato2
+
+var dato1 = 2;
+var dato2 = 2;
+var res= dato1+dato2
+console.log(res);
+*/
---
+++ Pagina/app3.js
@@ -0,0 +1,25 @@
+var dato1 = parseInt(prompt("Ingresa número 1"))
+var dato2 = parseInt(prompt("Ingresa número 2"))
+
+console.log("Tu primer número es " + dato1)
+console.log("Tu segundo número es " + dato2)
+console.log("Suma de tus números es " + (dato1 + dato2))
+console.log("Resta de tus números es " + (dato1 - dato2))
+
+var igual1 =parseInt = 88
+var igual2 =parseInt = 88
+var resultado = igual1 == igual2;
+console.log("Tus numeros son "+igual1+ " "+ igual2+ " la igualdad es " +resultado )
+
+//true
+console.log(resultado);
+
+var resultado =20 !== 10;
+console.log(resultado);
+
+// &&: Si los dos son verdaeros devuelve verdadero
+// ||: Con uno que sea verdaero es verdadero
+// ! : negacion
+
+var compara =10 < 8 && 10 ==10 ;
+console.log(compara)
