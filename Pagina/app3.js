ba06058f5014480aa720e792964ea4c7c6d12370
marcosilva321-123 <marcosilva321@aragon.unam.mx>
2026-02-25 20:10:36 -0600

    commit

--- Pagina/app3.js
+++ Pagina/app3.js
@@ -1,10 +1,19 @@
 var dato1 = parseInt(prompt("Ingresa número 1"))
 var dato2 = parseInt(prompt("Ingresa número 2"))
+var nombre = prompt("Ingresa tu nombre")
+var edad = parseInt(prompt("Ingresa tu edad"))
+var carrerra = prompt("Ingresa tu carrera")
+var correo = prompt("Ingresa tu correo")
+
 
 console.log("Tu primer número es " + dato1)
 console.log("Tu segundo número es " + dato2)
 console.log("Suma de tus números es " + (dato1 + dato2))
 console.log("Resta de tus números es " + (dato1 - dato2))
+console.log("Multiplicacion de tus números es " + (dato1 * dato2))
+console.log("Modulo de tus números es " + (dato1 % dato2))
+
+console.log("Tu nombre es "+nombre+", tu edad es "+edad+", tu carrera es "+carrerra+", tu correo es "+correo+".")
 
 var igual1 =parseInt = 88
 var igual2 =parseInt = 88
@@ -23,3 +32,12 @@ console.log(resultado);
 
 var compara =10 < 8 && 10 ==10 ;
 console.log(compara)
+
+let mensaje = prompt("Ingresa tu pass");
+if(mensaje === "123456"){
+    console.log("Genial :");
+}else{
+    console.log("Error pass mal :(");
+}
+
+console.log ("Termina el progreso.......")
